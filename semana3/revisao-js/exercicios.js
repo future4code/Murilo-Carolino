bec1ae43c4c14d222deec83338d8507e166224c6
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-08-23 15:25:20 -0300

    Exercício 12 concluído

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -158,7 +158,11 @@ function retornaChamadaDeFilme(filme) {
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-
+    let novaPessoa = {
+        ...pessoa,
+        nome: "ANÔNIMO"
+    }
+    return novaPessoa
 }
 
 // EXERCÍCIO 13A
