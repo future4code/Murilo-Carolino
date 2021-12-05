c0d5eff2d8b860d8a29042581eda6ff31289cf70
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-12-05 20:00:19 -0300

    Desfio 1 - Busca por todos os produtos e order

--- semana17/projeto-LabECommerce-BackEnd/src/endpoints/getAllProducts.ts
+++ semana17/projeto-LabECommerce-BackEnd/src/endpoints/getAllProducts.ts
@@ -5,7 +5,12 @@ import { Product } from "../types";
 export const getAllProducts = async (req: Request, res: Response): Promise<void> => {
     try {
 
+        const order = req.query.order === "desc"? "desc" : "asc"
+        const search = req.query.search || "%"
+
         const products: Product[] = await connection("labecommerce_products")
+            .orderBy("name", order)
+            .where("name", "LIKE", `%${search}%`)
         
         res.status(200).send(products)
 
