19daa7c6bb5cfe58bd92708521b14e5d7368815c
Murilo Terenciani <murilo.terenciani@gmail.com>
2022-01-10 19:35:11 -0300

    Desafios 4 e 5

--- semana21/testesAutomatizados-typescript/src/index.ts
+++ semana21/testesAutomatizados-typescript/src/index.ts
@@ -1,6 +1,7 @@
 import knex from 'knex'
 import { config } from 'dotenv'
 import { User } from './model/User'
+import { Casino, LOCATION, NACIONALITY, Result, UserCasino } from './model/Casino'
 
 config()
 
@@ -16,6 +17,56 @@ export const performPurchase = (user: User, value: number) => {
    }
 
    return undefined
+}
+
+export const verifyAge = (casino: Casino, users: UserCasino[]): Result => {
+
+   const allowed: UserCasino[] = []
+   const unallowed: UserCasino[] = []
+
+   for (const user of users) {
+      if (casino.location === LOCATION.BRAZIL) {
+         if (user.age >= 18) {
+            allowed.push(user)
+         } else {
+            unallowed.push(user)
+         }
+      } else if (casino.location === LOCATION.EUA) {
+         if (user.age >= 21) {
+            allowed.push(user)
+         } else {
+            unallowed.push(user)
+         }
+      }
+   }
+
+   return {
+      brazilians: {
+         allowed: allowed.filter((user) => {
+            return user.nacionality === NACIONALITY.BRAZILIAN
+         }).map((user) => {
+            return user.name
+         }),
+         unallowed: unallowed.filter((user) => {
+            return user.nacionality === NACIONALITY.BRAZILIAN
+         }).map((user) => {
+            return user.name
+         })
+      },
+      americans: {
+         allowed: allowed.filter((user) => {
+            return user.nacionality === NACIONALITY.AMERICAN
+         }).map((user) => {
+            return user.name
+         }),
+         unallowed: unallowed.filter((user) => {
+            return user.nacionality === NACIONALITY.AMERICAN
+         }).map((user) => {
+            return user.name
+         })
+      }
+      
+   }
 }
 
 export const connection = knex({
--- semana21/testesAutomatizados-typescript/tests/index.test.ts
+++ semana21/testesAutomatizados-typescript/tests/index.test.ts
@@ -1,7 +1,8 @@
-import { performPurchase } from "../src"
+import { performPurchase, verifyAge } from "../src"
+import { Casino, LOCATION, NACIONALITY, UserCasino } from "../src/model/Casino"
 import { User } from "../src/model/User"
 
-describe("index.ts", () => {
+describe("Exercícios aula 62", () => {
 
     test("Balance greater than value", () => {
 
@@ -44,4 +45,222 @@ describe("index.ts", () => {
 
         expect(result).not.toBeDefined()
     })
+})
+
+describe("Desafio 3/4 - Casino", () => {
+
+    test("a Brazilian user who can enter the casino in Brazil", () => {
+
+        const users: UserCasino[] = [{
+            name: "Murilo",
+            age: 30,
+            nacionality: NACIONALITY.BRAZILIAN
+        }]
+
+        const casino: Casino = {
+            name: "Joga Cassino",
+            location: LOCATION.BRAZIL
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.allowed).toEqual(["Murilo"])
+    })
+
+    test("An American user who can enter the casino in Brazil", () => {
+
+        const users: UserCasino[] = [{
+            name: "Johnny",
+            age: 25,
+            nacionality: NACIONALITY.AMERICAN
+        }]
+
+        const casino: Casino = {
+            name: "Joga Cassino",
+            location: LOCATION.BRAZIL
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.americans.allowed).toEqual(["Johnny"])
+    })
+
+    test("All unallowed", () => {
+
+        const users: UserCasino[] = [
+            {
+                name: "Johnny",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            }, {
+                name: "Anne",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Paulo",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Amanda",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            }
+        ]
+
+        const casino: Casino = {
+            name: "Play Casino",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.unallowed).toEqual(["Paulo", "Amanda"])
+        expect(result.americans.unallowed).toEqual(["Johnny", "Anne"])
+    })
+
+    test("Two americans allowed and two brazilians unallowed", () => {
+
+        const users: UserCasino[] = [
+            {
+                name: "Johnny",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            }, {
+                name: "Anne",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Paulo",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Amanda",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            }
+        ]
+
+        const casino: Casino = {
+            name: "Play Casino",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.unallowed).toEqual(["Paulo", "Amanda"])
+        expect(result.americans.allowed).toEqual(["Johnny", "Anne"])
+    })
+
+    test("Brazilian allowed", () => {
+
+        const users: UserCasino[] = [{
+            name: "Murilo",
+            age: 30,
+            nacionality: NACIONALITY.BRAZILIAN
+        }]
+
+        const casino: Casino = {
+            name: "Joga Cassino",
+            location: LOCATION.BRAZIL
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.allowed.length).toBeLessThan(2)
+        expect(result.brazilians.allowed.length).toBeGreaterThan(0)
+    })
+
+    test("American allowed", () => {
+
+        const users: UserCasino[] = [{
+            name: "Johnny",
+            age: 25,
+            nacionality: NACIONALITY.AMERICAN
+        }]
+
+        const casino: Casino = {
+            name: "Joga Cassino",
+            location: LOCATION.BRAZIL
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.americans.unallowed.length).toEqual(0)
+    })
+
+    test("All unallowed", () => {
+
+        const users: UserCasino[] = [
+            {
+                name: "Johnny",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            }, {
+                name: "Anne",
+                age: 19,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Paulo",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Amanda",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            }
+        ]
+
+        const casino: Casino = {
+            name: "Play Casino",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.unallowed).toContain("Paulo")
+        expect(result.americans.unallowed).toContainEqual("Johnny")
+    })
+
+    test("Two americans allowed and two brazilians unallowed", () => {
+
+        const users: UserCasino[] = [
+            {
+                name: "Johnny",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            }, {
+                name: "Anne",
+                age: 21,
+                nacionality: NACIONALITY.AMERICAN
+            },
+            {
+                name: "Paulo",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            },
+            {
+                name: "Amanda",
+                age: 19,
+                nacionality: NACIONALITY.BRAZILIAN
+            }
+        ]
+
+        const casino: Casino = {
+            name: "Play Casino",
+            location: LOCATION.EUA
+        }
+
+        const result = verifyAge(casino, users)
+
+        expect(result.brazilians.unallowed.length).toBeGreaterThan(1)
+        expect(result.americans.unallowed.length).toBeLessThan(1)
+        expect(result.americans.allowed.length).toEqual(2)
+    })
+
 })
